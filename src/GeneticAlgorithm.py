c413f3b908b1f18565329f895e627d8586d72175
riyorax <maximiliansulistiyo@gmail.com>
2024-11-04 12:32:43 +0700

    feat : add multiprocessing

--- src/GeneticAlgorithm.py
+++ src/GeneticAlgorithm.py
@@ -1,5 +1,6 @@
 import random
 import heapq
+import multiprocessing as mp
 import Cube as c
 
 def mutate(child, mutation_rate=0.01):
@@ -8,7 +9,6 @@ def mutate(child, mutation_rate=0.01):
         i, j = random.sample(range(size), 2)
         child.array[i], child.array[j] = child.array[j], child.array[i]
     child.calculate_state_value()
-    # print('mutate')
     return child
 
 def crossover(parent1, parent2):
@@ -43,6 +43,13 @@ def roulette_wheel_selection(population):
         if current > pick:
             return individual
 
+def create_child_task(args):
+    parent1, parent2, mutation_rate = args
+    child1, child2 = crossover(parent1, parent2)
+    child1 = mutate(child1, mutation_rate)
+    child2 = mutate(child2, mutation_rate)
+    return child1 if child1.state_value > child2.state_value else child2
+
 def genetic(N=100, max_generations=1000, initial_mutation_rate=0.05, stagnation_limit=100):
     population = [c.Cube(5, 5, 5, True) for _ in range(N)]
     current_max_fit = max(population, key=lambda x: x.state_value)
@@ -51,53 +58,44 @@ def genetic(N=100, max_generations=1000, initial_mutation_rate=0.05, stagnation_
     mutation_rate = initial_mutation_rate
     elite_size = int(0.05 * N)
     cubes = []
-    for generation in range(max_generations):
-        new_population = heapq.nlargest(elite_size, population, key=lambda x: x.state_value)
-        
 
-        if no_improvement_count > stagnation_limit / 2:
-            mutation_rate = min(0.3, mutation_rate * 1.2)
-        else:
-            mutation_rate = initial_mutation_rate
+    with mp.Pool(mp.cpu_count()) as pool:
+        for generation in range(max_generations):
+            new_population = heapq.nlargest(elite_size, population, key=lambda x: x.state_value)
 
-        while len(new_population) < N:
-            parent1, parent2 = roulette_wheel_selection(population), roulette_wheel_selection(population)
-            child1, child2 = crossover(parent1, parent2)
-            child1 = mutate(child1, mutation_rate)
-            child2 = mutate(child2, mutation_rate)
-            best_child = child1 if child1.state_value > child2.state_value else child2
-            
-            new_population.append(best_child)
 
-        population = heapq.nlargest(N, new_population, key=lambda x: x.state_value)
-        best_child = population[0]
-        print(f"largest {population[0].state_value}")
-        print(f"smallest {population[-1].state_value}")
+            mutation_rate = min(0.3, mutation_rate * 1.2) if no_improvement_count > stagnation_limit / 2 else initial_mutation_rate
 
-        if best_child.state_value > current_max_fit.state_value:
-            current_max_fit = best_child
-            no_improvement_count = 0
-        else:
-            no_improvement_count += 1
+            tasks = [
+                (roulette_wheel_selection(population), roulette_wheel_selection(population), mutation_rate)
+                for _ in range((N - elite_size) // 2)
+            ]
 
-        print(f"Generation {generation}: Current max fitness = {current_max_fit.state_value}")
+            children = pool.map(create_child_task, tasks)
+            new_population.extend(children)
 
-        if no_improvement_count >= stagnation_limit:
-            print("STAGNATION ALERT !!!")
-            similarity_count = 0
-            first_five_arrays = [ind.array for ind in population[:5]]
-            for i in range(4):
-                for j in range(i + 1, 5):
-                    if first_five_arrays[i] == first_five_arrays[j]:
-                        similarity_count += 1
-            
-            random_individuals = [c.Cube(5, 5, 5, True) for _ in range(int(0.3 * N))]
-            population = population[:int(0.7 * N)] + random_individuals
-            no_improvement_count = 0
+            population = heapq.nlargest(N, new_population, key=lambda x: x.state_value)
+            best_child = population[0]
 
-        values.append(current_max_fit.state_value)
-        cubes.append(current_max_fit)
+            if best_child.state_value > current_max_fit.state_value:
+                current_max_fit = best_child
+                no_improvement_count = 0
+            else:
+                no_improvement_count += 1
 
-    print("Final solution:")
-    current_max_fit.print_cube()
+            if(generation%100 == 0):
+                print(f"Generation {generation}: Current max fitness = {current_max_fit.state_value}")
+
+            if no_improvement_count >= stagnation_limit:
+                print("STAGNATION ALERT !!!")
+                random_individuals = [c.Cube(5, 5, 5, True) for _ in range(int(0.5 * N))]
+                population = population[:int(0.5 * N)] + random_individuals
+                no_improvement_count = 0
+
+            values.append(current_max_fit.state_value)
+            cubes.append(current_max_fit)
+
+        print("Final solution:")
+        current_max_fit.print_cube()
+
     return current_max_fit, values, cubes, generation + 1
--- src/Test.py
+++ src/Test.py
@@ -19,201 +19,202 @@ def to_3d_array(cube_1d, x_size=5, y_size=5, z_size=5):
         cube_3d.append(layer)
     
     return cube_3d
-  
-# Controlled test
-init_cube1 = [[[i+(5*j)+(25*k) for i in range(1,6)] for j in range(0,5)] for k in range(0,5)]
-init_cube2 = [[[((i+(5*j)+(25*k)+42)%125 + 1) for i in range(1,6)] for j in range(0,5)] for k in range(0,5)]
-init_cube3 = [[[((i+(5*j)+(25*k)+84)%125 + 1) for i in range(1,6)] for j in range(0,5)] for k in range(0,5)]
-magic_cube_3d_perfect = [
-    # Layer 1
-    [
-        [25, 16, 80, 104, 90],
-        [115, 98, 4, 1, 97],
-        [42, 111, 85, 2, 75],
-        [66, 72, 27, 102, 48],
-        [67, 18, 119, 106, 5]
-    ],
+
+if __name__ == "__main__":  
+  # Controlled test
+  init_cube1 = [[[i+(5*j)+(25*k) for i in range(1,6)] for j in range(0,5)] for k in range(0,5)]
+  init_cube2 = [[[((i+(5*j)+(25*k)+42)%125 + 1) for i in range(1,6)] for j in range(0,5)] for k in range(0,5)]
+  init_cube3 = [[[((i+(5*j)+(25*k)+84)%125 + 1) for i in range(1,6)] for j in range(0,5)] for k in range(0,5)]
+  magic_cube_3d_perfect = [
+      # Layer 1
+      [
+          [25, 16, 80, 104, 90],
+          [115, 98, 4, 1, 97],
+          [42, 111, 85, 2, 75],
+          [66, 72, 27, 102, 48],
+          [67, 18, 119, 106, 5]
+      ],
+      
+      # Layer 2
+      [
+          [91, 77, 71, 6, 70],
+          [52, 64, 117, 69, 13],
+          [30, 118, 21, 123, 23],
+          [26, 39, 92, 44, 114],
+          [116, 17, 14, 73, 95]
+      ],
+      
+      # Layer 3
+      [
+          [47, 61, 45, 76, 86],
+          [107, 43, 38, 33, 94],
+          [89, 68, 63, 58, 37],
+          [32, 93, 88, 83, 19],
+          [40, 50, 81, 65, 79]
+      ],
+      
+      # Layer 4
+      [
+          [31, 53, 112, 109, 10],
+          [12, 82, 34, 87, 100],
+          [103, 3, 105, 8, 96],
+          [113, 57, 9, 62, 74],
+          [56, 120, 55, 49, 35]
+      ],
+      
+      # Layer 5
+      [
+          [121, 108, 7, 20, 59],
+          [29, 28, 122, 125, 11],
+          [51, 15, 41, 124, 84],
+          [78, 54, 99, 24, 60],
+          [36, 110, 46, 22, 101]
+      ]
+  ]
+  cubes = [init_cube1, init_cube2, init_cube3, magic_cube_3d_perfect]
+  cubes_flatten = [np.array(init_cube1).flatten().tolist(),
+                  np.array(init_cube2).flatten().tolist(),
+                  np.array(init_cube3).flatten().tolist(),
+                  np.array(magic_cube_3d_perfect).flatten().tolist()]
+
+  # Test, Pick cube and algorithm as you see fit
+  cube_version = 0
+      # 0 = init_cube1 
+      # 1 = init_cube2
+      # 2 = init_cube3
+      # 3 = magic_cube_3d_perfect
     
-    # Layer 2
-    [
-        [91, 77, 71, 6, 70],
-        [52, 64, 117, 69, 13],
-        [30, 118, 21, 123, 23],
-        [26, 39, 92, 44, 114],
-        [116, 17, 14, 73, 95]
-    ],
+  test_cube_3d = cubes[cube_version]
+  test_cube_1d = cubes_flatten[cube_version]
     
-    # Layer 3
-    [
-        [47, 61, 45, 76, 86],
-        [107, 43, 38, 33, 94],
-        [89, 68, 63, 58, 37],
-        [32, 93, 88, 83, 19],
-        [40, 50, 81, 65, 79]
-    ],
+  # Pick algorithm
+  algorithm = 4
+      # 0 = Hill Climbing sideways
+      # 1 = Hill Climbing steepest ascent
+      # 2 = Hill Climbing random restart
+      # 3 = Simulated Annealing
+      # 4 = Genetic Algorithm
+      # 5 = Stochastic Hill Climbing Algorithm
+
+  # Note : The results are done with init_cube1
+
+  #  ===============================================================================================================================
+  # Hill climbing Sideways Move Test ===============================================================================================
+  if algorithm == 0:
     
-    # Layer 4
-    [
-        [31, 53, 112, 109, 10],
-        [12, 82, 34, 87, 100],
-        [103, 3, 105, 8, 96],
-        [113, 57, 9, 62, 74],
-        [56, 120, 55, 49, 35]
-    ],
+    print('New code test hill climbing sideways')
+    start_time = time.time()
+    cubes_new, values_new, count_iter_new = HillClimbingWithSidewaysMoveCube(test_cube_1d, 100)
+    end_time = time.time()
+    print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
+    # Result: 
+    # Current val: 45 | Neighbor Val: 45 | Sideways Count: 100
+    # Execution Time (New Code): 108.7507 seconds
     
-    # Layer 5
-    [
-        [121, 108, 7, 20, 59],
-        [29, 28, 122, 125, 11],
-        [51, 15, 41, 124, 84],
-        [78, 54, 99, 24, 60],
-        [36, 110, 46, 22, 101]
-    ]
-]
-cubes = [init_cube1, init_cube2, init_cube3, magic_cube_3d_perfect]
-cubes_flatten = [np.array(init_cube1).flatten().tolist(),
-                np.array(init_cube2).flatten().tolist(),
-                np.array(init_cube3).flatten().tolist(),
-                np.array(magic_cube_3d_perfect).flatten().tolist()]
+    print('Old code test hill climbing sideways')
+    start_time = time.time()
+    cubes_old, values_old, count_iter_old = HillClimbingWithSidewaysMove(test_cube_3d, 100)
+    end_time = time.time()
+    print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
+    # Result:
+    # Current val:  -75   |  Neighbor Val:  -75   |  Sideways Count:  100
+    # Execution Time (Old Code): 2094.5024 seconds
 
-# Test, Pick cube and algorithm as you see fit
-cube_version = 0
-    # 0 = init_cube1 
-    # 1 = init_cube2
-    # 2 = init_cube3
-    # 3 = magic_cube_3d_perfect
-  
-test_cube_3d = cubes[cube_version]
-test_cube_1d = cubes_flatten[cube_version]
-  
-# Pick algorithm
-algorithm = 4
-    # 0 = Hill Climbing sideways
-    # 1 = Hill Climbing steepest ascent
-    # 2 = Hill Climbing random restart
-    # 3 = Simulated Annealing
-    # 4 = Genetic Algorithm
-    # 5 = Stochastic Hill Climbing Algorithm
+  #  =================================================================================================================================
+  # Hill climbing Steepest Ascent Test ===============================================================================================
 
-# Note : The results are done with init_cube1
-
-#  ===============================================================================================================================
-# Hill climbing Sideways Move Test ===============================================================================================
-if algorithm == 0:
-  
-  print('New code test hill climbing sideways')
-  start_time = time.time()
-  cubes_new, values_new, count_iter_new = HillClimbingWithSidewaysMoveCube(test_cube_1d, 100)
-  end_time = time.time()
-  print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
-  # Result: 
-  # Current val: 45 | Neighbor Val: 45 | Sideways Count: 100
-  # Execution Time (New Code): 108.7507 seconds
-  
-  print('Old code test hill climbing sideways')
-  start_time = time.time()
-  cubes_old, values_old, count_iter_old = HillClimbingWithSidewaysMove(test_cube_3d, 100)
-  end_time = time.time()
-  print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
-  # Result:
-  # Current val:  -75   |  Neighbor Val:  -75   |  Sideways Count:  100
-  # Execution Time (Old Code): 2094.5024 seconds
-
-#  =================================================================================================================================
-# Hill climbing Steepest Ascent Test ===============================================================================================
-
-if algorithm == 1 :
-  print('New code test hill climbing steepest ascent')
-  start_time = time.time()
-  cubes_new, values_new, count_iter_new = SteepestAscentHillClimbingCube(test_cube_1d)
-  end_time = time.time()
-  print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
-  # Result:
-  # Current val:  44
-  # Execution Time (New Code): 23.9999 second
-  
-  print('Old code test hill climbing steepest ascent')
-  start_time = time.time()
-  cubes_old, values_old, count_iter_old = SteepestAscentHillClimbing(test_cube_3d)
-  end_time = time.time()
-  print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
-  # Result :
-  # Current val:  -76
-  # Execution Time (Old Code): 351.5430 seconds
-
-#  =================================================================================================================================
-# Hill climbing Random Restart Test ===============================================================================================
-
-if algorithm == 2 :
-  print('New code test hill climbing Random Restart')
-  start_time = time.time()
-  cubes_per_restart_new, values_per_restart_new, iteration_per_restart_new, restart_new= RandomRestartHillClimbingCube(test_cube_1d, 3)
-  end_time = time.time()
-  print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
-  # Result: 
-  # Execution Time (New Code): 99.3183 seconds
-  
-  
-  print('Old code test hill climbing steepest ascent')
-  start_time = time.time()
-  cubes_per_restart_old, values_per_restart_old, iteration_per_restart_old, restart_old = RandomRestartHillClimbing(test_cube_3d, 3)
-  end_time = time.time()
-  print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
-  # Result : 
-  # Too long....
-
-#  =================================================================================================================================
-# Simulated Annealing Test  ===============================================================================================
-
-if algorithm == 3 :
-  print('New code test Simulated Annealing')
-  start_time = time.time()
-  cubes_new, values_new, count_iter_new, e_probs_new, count_stuck_new = SimulatedAnnealingCube(test_cube_1d, 10000, 0.99)
-  end_time = time.time()
-  print(f'Value {values_new[-1]}')
-  print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
-  # Result:
-  
-  print('Old code test Simulated Annealing')
-  start_time = time.time()
-  cubes_old, values_old, count_iter_old, e_probs_old, count_stuck_old = SimulatedAnnealing(test_cube_3d, 10000, 0.99)
-  end_time = time.time()
-  print(f'Value {values_old[-1]}')
-  print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
-  # Result : 
-  
-#  =================================================================================================================================
-# Genetic Algorithm Test ===============================================================================================
-
-if algorithm == 4 :
-  print('Genetic Algorithm')
-  start_time = time.time()
-  current_max_fit, values, cubes, count_iter = genetic(900,3000)
-  end_time = time.time()
-  print(f'Value {values[-1]}')
-  print(f'Execution Time: {end_time - start_time:.4f} seconds')
-  
-#  =================================================================================================================================
-# Stochastic Algorithm Test ===============================================================================================
-
-if algorithm == 5 :
-  print('New code test Stochastic Hill Climbing')
-  start_time = time.time()
-  cubes_new, values_new, iter_new = StochasticHillClimbingCube(test_cube_1d, 1000)
-  end_time = time.time()
-  print(f'Value {values_new[-1]}')
-  print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
-  # Result:
-  # Value 20
-  # Execution Time (New Code): 0.4083 seconds
+  if algorithm == 1 :
+    print('New code test hill climbing steepest ascent')
+    start_time = time.time()
+    cubes_new, values_new, count_iter_new = SteepestAscentHillClimbingCube(test_cube_1d)
+    end_time = time.time()
+    print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
+    # Result:
+    # Current val:  44
+    # Execution Time (New Code): 23.9999 second
     
-  print('Old code test Stochastic Hill Climbing')
-  start_time = time.time()
-  cubes_old, values_old, iter_old = StochasticHillClimbing(test_cube_3d, 1000)
-  end_time = time.time()
-  print(f'Value {values_old[-1]}')
-  print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
-  # Result : 
-  # Value -96
-  # Execution Time (Old Code): 0.4848 seconds
-  
+    print('Old code test hill climbing steepest ascent')
+    start_time = time.time()
+    cubes_old, values_old, count_iter_old = SteepestAscentHillClimbing(test_cube_3d)
+    end_time = time.time()
+    print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
+    # Result :
+    # Current val:  -76
+    # Execution Time (Old Code): 351.5430 seconds
+
+  #  =================================================================================================================================
+  # Hill climbing Random Restart Test ===============================================================================================
+
+  if algorithm == 2 :
+    print('New code test hill climbing Random Restart')
+    start_time = time.time()
+    cubes_per_restart_new, values_per_restart_new, iteration_per_restart_new, restart_new= RandomRestartHillClimbingCube(test_cube_1d, 3)
+    end_time = time.time()
+    print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
+    # Result: 
+    # Execution Time (New Code): 99.3183 seconds
+    
+    
+    print('Old code test hill climbing steepest ascent')
+    start_time = time.time()
+    cubes_per_restart_old, values_per_restart_old, iteration_per_restart_old, restart_old = RandomRestartHillClimbing(test_cube_3d, 3)
+    end_time = time.time()
+    print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
+    # Result : 
+    # Too long....
+
+  #  =================================================================================================================================
+  # Simulated Annealing Test  ===============================================================================================
+
+  if algorithm == 3 :
+    print('New code test Simulated Annealing')
+    start_time = time.time()
+    cubes_new, values_new, count_iter_new, e_probs_new, count_stuck_new = SimulatedAnnealingCube(test_cube_1d, 10000, 0.99)
+    end_time = time.time()
+    print(f'Value {values_new[-1]}')
+    print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
+    # Result:
+    
+    print('Old code test Simulated Annealing')
+    start_time = time.time()
+    cubes_old, values_old, count_iter_old, e_probs_old, count_stuck_old = SimulatedAnnealing(test_cube_3d, 10000, 0.99)
+    end_time = time.time()
+    print(f'Value {values_old[-1]}')
+    print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
+    # Result : 
+    
+  #  =================================================================================================================================
+  # Genetic Algorithm Test ===============================================================================================
+
+  if algorithm == 4 :
+    print('Genetic Algorithm')
+    start_time = time.time()
+    current_max_fit, values, cubes, count_iter = genetic(900,3000)
+    end_time = time.time()
+    print(f'Value {values[-1]}')
+    print(f'Execution Time: {end_time - start_time:.4f} seconds')
+    
+  #  =================================================================================================================================
+  # Stochastic Algorithm Test ===============================================================================================
+
+  if algorithm == 5 :
+    print('New code test Stochastic Hill Climbing')
+    start_time = time.time()
+    cubes_new, values_new, iter_new = StochasticHillClimbingCube(test_cube_1d, 1000)
+    end_time = time.time()
+    print(f'Value {values_new[-1]}')
+    print(f'Execution Time (New Code): {end_time - start_time:.4f} seconds')
+    # Result:
+    # Value 20
+    # Execution Time (New Code): 0.4083 seconds
+      
+    print('Old code test Stochastic Hill Climbing')
+    start_time = time.time()
+    cubes_old, values_old, iter_old = StochasticHillClimbing(test_cube_3d, 1000)
+    end_time = time.time()
+    print(f'Value {values_old[-1]}')
+    print(f'Execution Time (Old Code): {end_time - start_time:.4f} seconds')
+    # Result : 
+    # Value -96
+    # Execution Time (Old Code): 0.4848 seconds
+    
