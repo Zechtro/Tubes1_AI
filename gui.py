412409fc7f9262c0c796c87b8f6ce4414dbb4f76
evelynnn04 <13522083@std.stei.itb.ac.id>
2024-11-04 09:36:27 +0700

    fix read json

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ from flet import Container, Slider, ElevatedButton, Row, Column, Image, Dropdown
 import base64
 import threading
 import time
+import json
 
 plt.switch_backend("Agg")
 
@@ -19,9 +20,10 @@ iter = 0
 total_iter = 0
 
 def create_dummy():
-    for i in range(10):
-        cube = np.random.randint(0, 100, size=(5, 5, 5))  
-        matrix_3d.append(cube)
+    global matrix_3d
+    cube = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125]
+    matrix_3d = []
+    matrix_3d.append(cube)
 
 def save_cube_to_file():
     global matrix_3d
@@ -34,12 +36,21 @@ def save_cube_to_file():
                 np.savetxt(f, layer, fmt='%d', delimiter=' ')
                 f.write("\n")
 
+def read_json_to_array():
+    global matrix_3d, total_iter
+    with open("state.json", "r") as file:
+        matrix_3d = json.load(file)
+    total_iter = len(matrix_3d)
+
+
 def update_plot():
     global iter, matrix_3d, global_min, global_max
     fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-    cube = matrix_3d[iter]
+
+    array_1d = np.array(matrix_3d[iter]).reshape(5, 5, 5)
+
     for i in range(5):
-        layer = cube[i, :, :]
+        layer = array_1d[i, :, :]
         
         row = i // 3
         col = i % 3
@@ -55,6 +66,7 @@ def update_plot():
 
     for j in range(i + 1, 6):
         fig.delaxes(axes.flatten()[j])
+    
     plt.tight_layout()
 
     buf = BytesIO()
@@ -103,9 +115,7 @@ def on_playback_speed_change(e):
     print("Playback Speed:", playback_speed)
 
 def main(page: ft.Page):
-    global total_iter
-    total_iter = 10 #ntar diganti aja
-    create_dummy() # Ini ntar diganti sama list of cube yg asli
+    read_json_to_array()
 
     play_pause_button = ElevatedButton(text="Play", on_click=on_play_pause_clicked, height=40)
     global progress_slider
@@ -128,12 +138,10 @@ def main(page: ft.Page):
         width=200, height=40
     )
 
-    load_button = ElevatedButton(text="Load File", on_click=lambda e: save_cube_to_file())
+    load_button = ElevatedButton(text="Load File", on_click=lambda e: save_cube_to_file)
 
     global plot_image
     plot_image = Image(src_base64=update_plot(), width=700, height=500)
-
-
 
     page.add(
         Column(height=20),
